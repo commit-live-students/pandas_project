a6ded7186d2478f493460390b386b3dbe5f9ffbd
Vishwas Singh <vishwassingh31@gmail.com>
2017-09-28 16:17:35 +0530

    updating single test case

--- q01_read_csv_data_to_df/build.py
+++ q01_read_csv_data_to_df/build.py
@@ -1,5 +1,6 @@
 # Mandatory Import
 import pandas as pd
-
+path = "..data/ipl_dataset.csv"
 def read_csv_data_to_df(path):
+
     return pd.read_csv(path)
---
+++ q01_read_csv_data_to_df/tests/test_q01_read_csv_data_to_df.py
@@ -0,0 +1,13 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
+from unittest import TestCase
+from q01_read_csv_data_to_df.build import read_csv_data_to_df
+import pandas
+
+class TestRead_csv_data_to_df(TestCase):
+    def test_read_csv_data_to_df(self):
+        path = "../data/ipl_dataset.csv"
+        df = read_csv_data_to_df(path)
+
+        self.assertIsInstance(df, pandas.DataFrame)
+        self.assertTrue(df.shape == (136522, 24))
