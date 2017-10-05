d987c53604c07cf20218f931cffca9adcb151315
Jay Trivedi <mail.jaytrivedi@gmail.com>
2017-10-05 20:07:40 +0530

    Update build.py

--- q07_get_run_counts_by_match/build.py
+++ q07_get_run_counts_by_match/build.py
@@ -5,7 +5,3 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
 # Solution
-def get_runs_counts_by_match():
-    runs_by_match = ipl_df.pivot_table(values='delivery', index='match_code', columns=['runs'],
-                                       aggfunc='count')
-    return runs_by_match
