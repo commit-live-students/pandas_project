7d84d4554ee7f18d74d8a4b44e2664cc7a03d982
sudharsanasai <Sudharsanasai@gmail.com>
2017-10-05 15:18:29 +0000

    Done

--- q07_get_run_counts_by_match/build.py
+++ q07_get_run_counts_by_match/build.py
@@ -6,6 +6,6 @@ ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
 # Solution
 def get_runs_counts_by_match():
-    runs_by_match = ipl_df.pivot_table(values='delivery', index='match_code', columns=['runs'],
+    runs_by_match = ipl_df.pivot_table(values = 'delivery',index='match_code', columns=['runs'],
                                        aggfunc='count')
     return runs_by_match
