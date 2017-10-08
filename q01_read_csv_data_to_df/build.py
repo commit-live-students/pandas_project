c7a4cd0b054fdc21fc8ceca740196e6b0b6fe3ba
Mohammed-Sunasra <sunasramohd@gmail.com>
2017-10-08 17:55:08 +0000

    Done

--- q01_read_csv_data_to_df/build.py
+++ q01_read_csv_data_to_df/build.py
@@ -4,5 +4,7 @@ import pandas as pd
 # Path has been given to you already to use in function.
 path = "data/ipl_dataset.csv"
 
-# Solution
+def read_csv_data_to_df(path):
+    return pd.read_csv(path)
 
+read_csv_data_to_df(path)
