ffcbceb21e32cd5a6777705e858dedc36a93b20d
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-09 07:03:26 +0000

    Done

--- q04_get_match_specific_df/build.py
+++ q04_get_match_specific_df/build.py
@@ -4,4 +4,10 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 # You have been given dataset already in 'ipl_df'.
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
-# Solution
+import pandas as pd
+def get_match_specific_df(match_code) :
+    #path = path
+    a = pd.read_csv("data/ipl_dataset.csv")
+    df = a[(a.match_code == match_code)]
+    #print a
+    return df
