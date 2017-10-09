056f29e4b8b89bb0c65264ae079f72d38566b0cc
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-09 06:52:49 +0000

    Done

--- q03_get_run_counts/build.py
+++ q03_get_run_counts/build.py
@@ -4,5 +4,14 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 # You have been given the dataset already in 'ipl_df'.
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
-# Solution
+import pandas as pd
+def get_run_counts() :
+    path = "data/ipl_dataset.csv"
+    #path = path
+    ipl_df = pd.read_csv(path)
+    runs_count = ipl_df['match_code'].groupby(ipl_df['runs']).count()
+    #unq = ipl_df[:]
+    return runs_count
 
+
+# Solution
