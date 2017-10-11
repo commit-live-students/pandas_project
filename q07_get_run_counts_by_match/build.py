c77f1e0e7008212e8c46d0ee7b2b3251342b4a51
pritambhaladhare <pritam.bhaladhare@gmail.com>
2017-10-11 17:47:19 +0000

    Done

--- q07_get_run_counts_by_match/build.py
+++ q07_get_run_counts_by_match/build.py
@@ -5,3 +5,14 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
 # Solution
+def get_runs_counts_by_match():
+    balls =[0,1,2,3,4,5,6]
+    match_code = ipl_df['match_code']
+    runs = ipl_df['runs']
+    pivot_df = ipl_df.pivot_table(
+                 values = 'runs',
+                 index = 'match_code',
+                 columns = ipl_df['runs'],
+                 aggfunc='count')
+    return pivot_df
+get_runs_counts_by_match()
