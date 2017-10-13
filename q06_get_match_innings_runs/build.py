770afb3e16a91013c65b50bc8f4748cb953e5e58
ashwinij9 <ashwinijoshi9793@gmail.com>
2017-10-13 08:02:55 +0000

    Done

--- q06_get_match_innings_runs/build.py
+++ q06_get_match_innings_runs/build.py
@@ -5,15 +5,14 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 ipl_df = read_csv_data_to_df("data/ipl_dataset.csv")
 import numpy as np
 
+ipl_df = read_csv_data_to_df("data/ipl_dataset.csv")
+import numpy as np
+import pandas as pd
 def get_match_innings_runs():
-    df = ipl_df.pivot_table(
-            values = "runs",
-            index = "match_code",
-            columns = "inning",
-            aggfunc=np.sum )
 
-    return df
+    df1 = ipl_df["runs"].groupby([ipl_df.match_code,ipl_df.inning]).agg(np.sum)
+    df2 = pd.DataFrame(df1)
+    return df2
 
 
 get_match_innings_runs()
-    
