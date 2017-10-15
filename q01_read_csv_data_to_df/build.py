de4be8e89c7d861fd0b3601fe68efedf343b1946
Sakshi-BUK <sakshi.dhokrat96@gmail.com>
2017-10-15 11:28:35 +0000

    Done

--- q01_read_csv_data_to_df/build.py
+++ q01_read_csv_data_to_df/build.py
@@ -1,8 +1,16 @@
+# %load q01_read_csv_data_to_df/build.py
 # Default Imports
 import pandas as pd
 
 # Path has been given to you already to use in function.
 path = "data/ipl_dataset.csv"
 
+def read_csv_data_to_df  (path):
+    df=pd.read_csv(path)
+    #print("Shape:",df.shape)
+    #print("Index:",df.index)
+    return df
+
 # Solution
 
+read_csv_data_to_df  (path)
