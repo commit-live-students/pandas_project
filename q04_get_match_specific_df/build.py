5f76a4cc6a5ff8f213e4f995d31ce21d9b8b20bf
darshind <doshi.darshin@gmail.com>
2017-11-11 08:52:50 +0000

    Done

--- q04_get_match_specific_df/build.py
+++ q04_get_match_specific_df/build.py
@@ -1,11 +1,11 @@
 # Default imports
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
-
+import pandas as pd
 # You have been given dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("/home/darshind/Workspace/code/pandas_project/data/ipl_dataset.csv")
+ipl_df = pd.read_csv("/home/darshind/Workspace/code/pandas_project/data/ipl_dataset.csv")
 
 # Solution
 def get_match_specific_df(match_code):
-    df = ipl_df[ipl_df['match_code'] == 392203]
-    return df
-print get_match_specific_df(match_code = 392203)
+    matc_specific = ipl_df[ipl_df['match_code'] == match_code]
+    return matc_specific
+#get_match_specific_df(match_code = '335983')
--- q07_get_run_counts_by_match/build.py
+++ q07_get_run_counts_by_match/build.py
@@ -1,7 +1,11 @@
 # Default Imports
-from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
-
+#from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
+import pandas as pd
 # You have been give the dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
+ipl_df = pd.read_csv("/home/darshind/Workspace/code/pandas_project/data/ipl_dataset.csv")
 
 # Solution
+def get_runs_counts_by_match():
+    filter_data = [['match_code','runs','inning']]
+    return pd.pivot_table(filter_data,columns = 'runs',index ='match_code',aggfunc='count')
+print get_runs_counts_by_match()
