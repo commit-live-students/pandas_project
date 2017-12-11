5a981fb343a18fb99213ce973465be9e8b9c103e
ms10031990 <ms10031990@yahoo.com>
2017-12-11 18:05:55 +0000

    Done

--- q04_get_match_specific_df/build.py
+++ q04_get_match_specific_df/build.py
@@ -1,7 +1,12 @@
-# Default imports
+import pandas as pd
+from pandas import DataFrame
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
 
 # You have been given dataset already in 'ipl_df'.
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
-# Solution
+#Use the Locate (.loc) feature/Function to extract details of a given match_code
+def get_match_specific_df(match_code):
+    df =  DataFrame(ipl_df.loc[ipl_df['match_code'] == match_code])
+
+    return df
