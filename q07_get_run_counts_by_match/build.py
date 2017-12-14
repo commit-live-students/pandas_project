23a57c91fc92aa7578ed5737503195651395ea91
ms10031990 <ms10031990@yahoo.com>
2017-12-14 16:18:13 +0000

    Done

--- q07_get_run_counts_by_match/build.py
+++ q07_get_run_counts_by_match/build.py
@@ -1,7 +1,13 @@
-# Default Imports
+import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
 
 # You have been give the dataset already in 'ipl_df'.
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
-# Solution
+# Use the pivot_table feature from pandas to slice column match_code and runs using .loc function and aggregate the occurences
+# of respective runs (columns = runs) using numpy's count function
+def get_runs_counts_by_match():
+    df = pd.pivot_table(ipl_df.loc[:,['match_code','runs']], index=['match_code'], columns = 'runs', aggfunc=np.count_nonzero)
+    return df
