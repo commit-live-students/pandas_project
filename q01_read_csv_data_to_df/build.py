d89369e2db67cf6875b1881f2791a9a079a7accd
vaithyanathanramanathan <vaithy31@gmail.com>
2017-12-18 18:04:50 +0000

    Done

--- q01_read_csv_data_to_df/build.py
+++ q01_read_csv_data_to_df/build.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 # Path has been given to you already to use in function.
 path = "data/ipl_dataset.csv"
-
-# Solution
-
+path1 = "data/ipl_dataset.csv"
+path2 = "data/weather_2012.csv"
+def read_csv_data_to_df(path):
+	return pd.read_csv(path)
