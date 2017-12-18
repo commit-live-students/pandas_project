793e8ff4452fa1ddb9e9a7587370c1434078e298
rnoshir <noshir92@gmail.com>
2017-12-18 17:39:06 +0000

    Done

--- q07_get_run_counts_by_match/build.py
+++ q07_get_run_counts_by_match/build.py
@@ -1,7 +1,11 @@
 # Default Imports
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
-
+import numpy as np
 # You have been give the dataset already in 'ipl_df'.
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
 # Solution
+def get_runs_counts_by_match():
+    data=ipl_df[['match_code','runs']]
+    data.loc[:,'count']=np.ones(1)
+    return data.pivot_table('count', aggfunc=np.sum, index='match_code', columns='runs').fillna(0)
