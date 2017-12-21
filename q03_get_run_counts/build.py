4b0be0886cf318ce929bf0bfb9b6076fb16b3aec
iamsathishsatz <Sathishbabu.s.g@gmail.com>
2017-12-21 19:30:49 +0000

    Done

--- q03_get_run_counts/build.py
+++ q03_get_run_counts/build.py
@@ -5,4 +5,8 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
 # Solution
-
+def get_run_counts():
+    pivot_tbl = ipl_df.pivot_table(
+                values= 'runs', index=ipl_df['runs'], aggfunc=lambda x: len(x))
+    return pivot_tbl['runs']
+#print type(get_run_counts())
