9a6a740fd308d48240f7ed1af0066b20864df428
brockLesnar37 <ashlin+37@greyatom.com>
2018-01-24 07:03:52 +0000

    Done

--- q04_get_match_specific_df/build.py
+++ q04_get_match_specific_df/build.py
@@ -1,7 +1,9 @@
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
 
-# You have been given dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
+# You have been given the dataset already in 'ipl_df'.
+ipl_df = read_csv_data_to_df("data/ipl_dataset.csv")
 
-# Solution
-
+#Solution
+def get_unique_venues():
+    venues = ipl_df['venue'].unique()
+    return venues
