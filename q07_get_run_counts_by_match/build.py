5c07e112cb497f8abb3edb3d2b6ed411e36014ae
axc888 <jha.ashish43@gmail.com>
2018-02-05 05:23:09 +0000

    Done

--- q07_get_run_counts_by_match/build.py
+++ q07_get_run_counts_by_match/build.py
@@ -5,3 +5,14 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
 # Solution
+def get_runs_counts_by_match():
+  ##  return ipl_df.info()
+
+    pivot_df = ipl_df.pivot_table(
+        values='total',      # We want to aggregate the values of which column?
+        index='match_code',       # We want to use which column as the new index?
+        columns=['runs'],   # We want to use the values of which column as the new columns? (optional)
+        aggfunc=len)  # What aggregation function to use ?
+
+# Notice that the result is a hierarchical index
+    return pivot_df
