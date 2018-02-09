a8c73ede2f8d8463809210281cb8933476f56aec
sohelahmadkhan <sohelahmadkhan000@gmail.com>
2018-02-09 15:25:18 +0000

    Done

--- q07_get_run_counts_by_match/build.py
+++ q07_get_run_counts_by_match/build.py
@@ -3,24 +3,26 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 import pandas as pd
 import numpy as np
 from pandas import Series,DataFrame
-from collections import defaultdict
 
 # You have been give the dataset already in 'ipl_df'.
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
 # Solution
+def get_runs_counts_by_match():
+
+    runs_df = ipl_df[['match_code', 'runs']]
+    runs_df['count'] = 1
+    runs_pivot = runs_df.pivot_table('count', aggfunc=np.sum, index='match_code', columns='runs').fillna('NA')
+    #runs_pivot = runs_df.pivot_table('count', aggfunc=np.sum, index='match_code', columns='runs')
+    return runs_pivot
+
+
+
 '''def get_runs_counts_by_match():
 
-    df1 = ipl_df[['match_code','runs']]
-    df = df1.pivot_table('runs',aggfunc = 'count',index = 'match_code',columns = 'runs')
+    df = pd.pivot_table(data=ipl_df.loc[:,['match_code', 'runs']], index='match_code', columns=ipl_df.loc[:,'runs'], aggfunc='count')
 
     return df'''
 
-def get_runs_counts_by_match():
 
-    df1 = pd.pivot_table(data=ipl_df.loc[:,['match_code', 'runs']], index='match_code', columns=ipl_df.loc[:,'runs'], aggfunc='count')
-
-    return df1
-
-
-#print get_runs_counts_by_match()
+print get_runs_counts_by_match()
