05605026b27e6dde2c2ab312881e9ff42ab890b5
Sharad30 <sharad30.ss@gmail.com>
2018-02-14 05:54:38 +0000

    Done

--- q04_get_match_specific_df/build.py
+++ q04_get_match_specific_df/build.py
@@ -3,5 +3,10 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 # You have been given dataset already in 'ipl_df'.
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
-# Solution
+def get_match_specific_df(match):
 
+    unique_match_count = ipl_df.match_code.value_counts()
+    match_length = unique_match_count[int(match)]
+    match_data = ipl_df.iloc[0:match_length,:]
+
+    return match_data
