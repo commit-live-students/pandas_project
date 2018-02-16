9ff8e90385a17a5d1a18c0246ef434891e9f55e6
inivikrant <inivikrant@gmail.com>
2018-02-16 09:14:51 +0000

    Done

--- q04_get_match_specific_df/build.py
+++ q04_get_match_specific_df/build.py
@@ -1,6 +1,17 @@
+# %load q04_get_match_specific_df/build.py
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
+import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame
 
 # You have been given dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
-
+ipl_df = read_csv_data_to_df('./data/ipl_dataset.csv')
 # Solution
+def get_match_specific_df(match_code):
+    code = ipl_df['match_code']
+    return(code)
+print()
+
+    
+
+
--- q05_create_bowler_filter/build.py
+++ q05_create_bowler_filter/build.py
@@ -1,7 +1,8 @@
 # Default imports
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
-
+import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame
 # You have been given dataset already in 'ipl_df'.
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
-
 # Solution
