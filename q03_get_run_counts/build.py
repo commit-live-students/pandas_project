c8ca4f3e6d6bf375494f42645d4f16eec8b02791
sonal7930 <sonal.chandra7930@gmail.com>
2018-02-23 10:58:35 +0000

    Done

--- q03_get_run_counts/build.py
+++ q03_get_run_counts/build.py
@@ -1,11 +1,7 @@
-# Default Imports
+import pandas as pd
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
-path = "./data/ipl_dataset.csv""
-# You have been given the dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
-
-print read_csv_data_to_df(path)
-
-'''def get_unique_venues():
-    venues = ipl_df['venue'].unique()
-    return venues'''
+path = "./data/ipl_dataset.csv"
+ipl_df = read_csv_data_to_df(path)
+def get_run_counts():
+    runs_count=pd.Series.value_counts(ipl_df['runs'])
+    return runs_count
