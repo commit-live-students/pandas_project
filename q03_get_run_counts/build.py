555f1001f95c52c7a3581870c4c9767fd2dbff92
mancbg <manasigund22@gmail.com>
2018-02-23 15:14:02 +0000

    Done

--- q03_get_run_counts/build.py
+++ q03_get_run_counts/build.py
@@ -5,4 +5,5 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
 # Solution
-
+def get_run_counts():
+    return ipl_df['runs'].value_counts()
