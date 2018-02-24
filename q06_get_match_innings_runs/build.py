40d1bc8bfc44135107aebe01fcf82e93a75f96bd
CharulataShelar <charulatashelar@gmail.com>
2018-02-24 18:20:22 +0000

    Done

--- q06_get_match_innings_runs/build.py
+++ q06_get_match_innings_runs/build.py
@@ -5,7 +5,9 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 ipl_df = read_csv_data_to_df("data/ipl_dataset.csv")
 
 # Solution
+def get_match_innings_runs():
+    a = ipl_df.groupby(['match_code', 'inning'])
+    count_gb = a['runs'].sum()
+    return count_gb
 
-
-
-
+get_match_innings_runs()
