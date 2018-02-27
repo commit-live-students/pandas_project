2c4151facc027d50b6a84afc2792a4728a9f1902
guptaaman92 <guptaaman92@gmail.com>
2018-02-27 03:27:43 +0000

    Done

--- q03_get_run_counts/build.py
+++ q03_get_run_counts/build.py
@@ -1,8 +1,14 @@
+import numpy as np
+import pandas as pd
 # Default Imports
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
 
 # You have been given the dataset already in 'ipl_df'.
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 
-# Solution
+def get_run_counts():
+    runs = ipl_df.runs
+    runs_counts = runs.value_counts(normalize= False, sort = True, ascending= False)
+    return runs_counts
 
+get_run_counts()
