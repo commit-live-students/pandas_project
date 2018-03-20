db309dbf84e40422b181775a56d53d94c8109078
inivikrant <inivikrant@gmail.com>
2018-03-20 11:00:14 +0000

    Done

--- q05_create_bowler_filter/build.py
+++ q05_create_bowler_filter/build.py
@@ -6,3 +6,6 @@ from pandas import Series, DataFrame
 # You have been given dataset already in 'ipl_df'.
 ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
 # Solution
+def create_bowler_filter(bowler):
+    return (ipl_df['bowler'] == bowler)
+print(create_bowler_filter('I Sharma'))
