c27a7257c908148d78f63b1959e44abfd3c136f2
zob19 <Zshaikh2388@gmail.com>
2018-05-04 01:02:20 +0000

    Done

--- q02_get_unique_values/build.py
+++ q02_get_unique_values/build.py
@@ -1,6 +1,12 @@
+# %load q02_get_unique_values/build.py
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
 
-# You have been given the dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("data/ipl_dataset.csv")
+import pandas as pd
+import numpy as np
 
-#Solution
+# You have been given the dataset already in 'ipl_df'.
+ipl_df = read_csv_data_to_df('data/ipl_dataset.csv')
+
+def get_unique_venues():
+    return ipl_df.venue.unique()
+
