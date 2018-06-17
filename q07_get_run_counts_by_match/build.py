0720d00d6084a667ba2efd59ca9392f523f7f07d
Sumeshks29 <sumeshks29@gmail.com>
2018-06-17 08:24:12 +0000

    Done

--- q07_get_run_counts_by_match/build.py
+++ q07_get_run_counts_by_match/build.py
@@ -1,7 +1,18 @@
+# %load q07_get_run_counts_by_match/build.py
 # Default Imports
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
-
+import pandas as pd
+import numpy as np
 # You have been give the dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
+ipl_df = read_csv_data_to_df('./data/ipl_dataset.csv')
 
 # Solution
+def get_runs_counts_by_match(bowler='I Sharma'):
+    ipl_df1 = pd.read_csv('./data/ipl_dataset.csv',index_col='match_code')
+    ipl_df2 = ipl_df.pivot_table(columns='runs', index='match_code',aggfunc='sum', values = 'batsman')
+    
+    return ipl_df2
+
+get_runs_counts_by_match(bowler='I Sharma').shape
+get_runs_counts_by_match()
+
