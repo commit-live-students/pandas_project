1a31fcd99e6b77ded509e1916369ad2099ebf436
nalinmshah <nalinmshah@gmail.com>
2018-07-31 16:29:39 +0000

    Done

--- q05_create_bowler_filter/build.py
+++ q05_create_bowler_filter/build.py
@@ -1,7 +1,42 @@
-# Default imports
-from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
+import pandas as pd
+import numpy as np
 
-# You have been given dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
+str_path='data/ipl_dataset.csv'
 
-# Solution
+def read_csv_data_to_df(path):
+	pd_df= pd.read_csv(path,  delimiter=',' )
+	return pd_df
+
+def get_unique_venues():
+	npArray = np.array (df['venue'].unique())
+	return npArray
+	
+def get_run_counts():
+	np_unqRunCount = np.array(df['runs'].value_counts()[df['runs'].unique()])
+	np_unqRuns = np.array(df['runs'].unique())
+	#print (np_unqRunCount)
+	#print (np_unqRuns)
+	print(' np_unqRunCount : ',np_unqRunCount[0:])
+	series_run_counts = pd.Series(data = np_unqRunCount[0:], index=np_unqRuns)
+	return series_run_counts
+
+
+def get_match_specific_df(iMatchCode):
+	df_MatchCode = df[df['match_code']==iMatchCode ]
+	print(df_MatchCode.shape)
+	return df_MatchCode
+
+
+def create_bowler_filter(strBowler):
+	series_Bowler = pd.Series(df['bowler'].str.contains(strBowler) == True)
+	return series_Bowler
+
+df=read_csv_data_to_df(str_path)
+#print (df.shape)
+npArray_Venues = get_unique_venues()
+#print (type(npArray_Venues))
+srs_run_counts = get_run_counts()
+get_match_specific_df(598057)
+ser_Bowler = create_bowler_filter('I Sharma')
+
+
