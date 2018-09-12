fafdf98f904986c36ddef31ca0e501c134cdd38e
aishkl <aishkl123@gmail.com>
2018-09-12 12:09:34 +0000

    Done

--- q06_get_match_innings_runs/build.py
+++ q06_get_match_innings_runs/build.py
@@ -1,11 +1,17 @@
+# %load q06_get_match_innings_runs/build.py
 # Default Imports
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
-
+import numpy as np
 # You have been given dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("data/ipl_dataset.csv")
+ipl_df = read_csv_data_to_df('data/ipl_dataset.csv')
 
 # Solution
+def get_match_innings_runs():
+    grouped=ipl_df.groupby(['inning','match_code']).sum()
+    df=grouped['runs']
+    return df
 
+get_match_innings_runs()
 
 
 
