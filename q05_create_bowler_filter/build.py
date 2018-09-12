11b60b6243d676b275f78d5727d9a67ca2a59b7e
aishkl <aishkl123@gmail.com>
2018-09-12 10:35:12 +0000

    Done

--- q05_create_bowler_filter/build.py
+++ q05_create_bowler_filter/build.py
@@ -1,7 +1,15 @@
+# %load q05_create_bowler_filter/build.py
 # Default imports
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
-
+import pandas as pd
 # You have been given dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
-
+ipl_df = read_csv_data_to_df('./data/ipl_dataset.csv')
+bowler=ipl_df['bowler']
 # Solution
+def create_bowler_filter(bowler):
+    bowler_filter=ipl_df['bowler']==bowler
+    return bowler_filter
+
+create_bowler_filter(bowler)
+
+
