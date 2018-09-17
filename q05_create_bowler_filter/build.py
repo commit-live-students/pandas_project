cc1f3d91a51ade3230f2c5247d8248597b933342
sunilhariharan <sunilhariharan@gmail.com>
2018-09-17 10:45:43 +0000

    Done

--- q05_create_bowler_filter/build.py
+++ q05_create_bowler_filter/build.py
@@ -4,8 +4,15 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 
 # You have been given dataset already in 'ipl_df'.
 ipl_df = read_csv_data_to_df('./data/ipl_dataset.csv')
-impo
+import pandas as pd
+import numpy as np
 # Solution
-
+def create_bowler_filter(bowler):
+    
+    s=pd.Series(ipl_df['bowler'])
+    series=s.isin(['I Sharma'])
+    
+    return series
+    
 
 
