1c500ec062e391df5b6c41f80c7578e36bbba54e
PoojaTatkare <poojatatkare8@gmail.com>
2018-10-11 20:03:24 +0000

    Done

--- q05_create_bowler_filter/build.py
+++ q05_create_bowler_filter/build.py
@@ -6,12 +6,12 @@ from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_da
 ipl_df = read_csv_data_to_df('./data/ipl_dataset.csv')
 
 # Solution
-def create_bowler_filter(bowler):
-    a= (ipl_df['bowler'] == 'I Sharma')
+def create_bowler_filter(bowler_name):
+    a= ipl_df['bowler'] == 'I Sharma'
 
-    print(a)
+    return a
     
-bowler= 'I Sharma'
-create_bowler_filter(bowler)
+bowler_name= 'I Sharma'
+create_bowler_filter(bowler_name)
 
 
