eefd79fd3d59ab0fdbca8973b55d406ea52d1d9a
sagark93 <sagarkulkarni093@gmail.com>
2019-01-12 08:41:32 +0000

    Done

--- q04_get_match_specific_df/build.py
+++ q04_get_match_specific_df/build.py
@@ -1,7 +1,20 @@
+# %load q04_get_match_specific_df/build.py
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
-
+import pandas as pd
 # You have been given dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
+ipl_df = read_csv_data_to_df('./data/ipl_dataset.csv')
 
 # Solution
+def get_match_specific_df(match_code):
+    s = pd.Series()
+    s = ipl_df.loc[ipl_df['match_code'] == match_code]
+    return s
+type(ipl_df['match_code'])
+ipl_df.loc[ipl_df['match_code'] == 392203]
+
+get_match_specific_df(598057)
+
+
+
+
 
--- q05_create_bowler_filter/build.py
+++ q05_create_bowler_filter/build.py
@@ -1,7 +1,47 @@
+# %load q05_create_bowler_filter/build.py
 # Default imports
+import pandas as pd
+import numpy as np
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
 
 # You have been given dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
+ipl_df = read_csv_data_to_df('./data/ipl_dataset.csv')
 
 # Solution
+
+
+
+def create_bowler_filter (name):
+    for x in ipl_df['bowler'] :
+        s = pd.Series( index = list(x))
+        logical_results = str(s) == name
+        #logical_results_series = pd.Series(np.arange(0,8),index=logical_results)
+        new = s[logical_results]
+        return new
+    #bow_array =pd.Series
+# for x in ipl_df['bowler']:
+ #       ipl_df['bowler'] == name
+  #      bow_array = pd.Series(True)
+   # return bow_array
+create_bowler_filter('AB Dinda')
+
+s = pd.Series(  index = list(ipl_df['bowler']))
+s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
