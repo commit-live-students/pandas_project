d40788d55fb1a1b64bd80114fdb1193ffcc96e80
sagark93 <sagarkulkarni093@gmail.com>
2019-01-18 10:16:01 +0000

    Done

--- q05_create_bowler_filter/build.py
+++ q05_create_bowler_filter/build.py
@@ -12,21 +12,25 @@ ipl_df = read_csv_data_to_df('./data/ipl_dataset.csv')
 
 
 def create_bowler_filter (name):
-    for x in ipl_df['bowler'] :
-        s = pd.Series( index = list(x))
-        logical_results = str(s) == name
+    for x in ipl_df['bowler']:
+        #s = pd.Series( index = list(x))
+        s = pd.Series(ipl_df['bowler'] == name)
+        #logical_results = str(s) == name
         #logical_results_series = pd.Series(np.arange(0,8),index=logical_results)
-        new = s[logical_results]
-        return new
+        #new = s[logical_results]
+        #return new
+        return s
     #bow_array =pd.Series
-# for x in ipl_df['bowler']:
- #       ipl_df['bowler'] == name
-  #      bow_array = pd.Series(True)
+#for x in ipl_df['bowler']:
+ #ipl_df['bowler'] == name
+  #bow_array = pd.Series(True)
    # return bow_array
-create_bowler_filter('AB Dinda')
+create_bowler_filter('I Sharma').shape
 
-s = pd.Series(  index = list(ipl_df['bowler']))
-s
+#if ipl_df['bowler'] == 'AB Dinda':
+ #   boolean_value = True
+#else:
+  #  boolean_value = False
 
 
 
