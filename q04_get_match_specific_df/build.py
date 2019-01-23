5bdeffbacb216409034748b4b6e3c1e76b6f5589
vaibhav-cric <vaibhavrane.cric@gmail.com>
2019-01-23 17:21:27 +0000

    Done

--- q04_get_match_specific_df/build.py
+++ q04_get_match_specific_df/build.py
@@ -1,7 +1,17 @@
+# %load q04_get_match_specific_df/build.py
 from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
 
 # You have been given dataset already in 'ipl_df'.
-ipl_df = read_csv_data_to_df("./data/ipl_dataset.csv")
+ipl_df = read_csv_data_to_df('./data/ipl_dataset.csv')
 
 # Solution
+import pandas as pd
+
+def get_match_specific_df(match_code):
+    
+    dfi=ipl_df.groupby('match_code').get_group(598057)
+    
+    return dfi
+
+
 
